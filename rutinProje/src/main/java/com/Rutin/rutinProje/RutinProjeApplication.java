cf21a37becdcb3f10e294aa01c48980999f721e3
Emir Taşdemir <100260548+Emrtasdemir@users.noreply.github.com>
2024-10-05 20:37:18 +0300

    Add files via upload

---
+++ rutinProje/src/main/java/com/Rutin/rutinProje/RutinProjeApplication.java
@@ -0,0 +1,13 @@
+package com.Rutin.rutinProje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutinProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutinProjeApplication.class, args);
+	}
+
+}
---
+++ rutinProje/src/test/java/com/Rutin/rutinProje/RutinProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rutin.rutinProje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutinProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
